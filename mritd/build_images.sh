4b3cce410202a09668d9d5ab2c3ddbdb9fdf1c6d
mritd <mritd@mritd.me>
2016-09-13 13:46:39 +0900

    增加不缓存编译

--- mritd/build_images.sh
+++ mritd/build_images.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-(cd idea && docker build -t mritd/idea .)
-(cd mritd && docker build -t mritd/mritd .)
-(cd nginx && docker build -t mritd/nginx .)
+if [ "$1" == "nocache" ];then
+  (cd idea && docker build --no-cache -t mritd/idea .) 
+  (cd mritd && docker build --no-cache -t mritd/mritd .)
+  (cd nginx && docker build --no-cache -t mritd/nginx .)
+else
+  (cd idea && docker build -t mritd/idea .)
+  (cd mritd && docker build -t mritd/mritd .)
+  (cd nginx && docker build -t mritd/nginx .)
+fi
+

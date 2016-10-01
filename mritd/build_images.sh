e093c46ff4cc3597ed210205141b269af796e538
mritd <mritd1234@gmail.com>
2016-10-01 16:54:53 +0800

    切换博客到 jekyll

--- mritd/build_images.sh
+++ mritd/build_images.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ "$1" == "nocache" ];then
+if [ "$1" == "-f" ];then
   (cd idea && docker build --no-cache -t mritd/idea .) 
-  (cd mritd && docker build --no-cache -t mritd/mritd .)
+  (cd jekyll && docker build --no-cache -t mritd/jekyll .)
   (cd nginx && docker build --no-cache -t mritd/nginx .)
 else
   (cd idea && docker build -t mritd/idea .)
-  (cd mritd && docker build -t mritd/mritd .)
+  (cd jekyll && docker build -t mritd/jekyll .)
   (cd nginx && docker build -t mritd/nginx .)
 fi
 

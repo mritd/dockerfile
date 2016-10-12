cc00baf3162c7b956d3e689fef93fb4ba2e6d054
mritd <mritd1234@gmail.com>
2016-10-12 22:50:30 +0800

    add other options

--- shadowsocks/entrypoint.sh
+++ shadowsocks/entrypoint.sh
@@ -6,11 +6,11 @@ PASSWORD=ZQoPF2g6uwJE7cy4
 METHOD=aes-256-cfb
 TIMEOUT=300
 ONE_TIME_AUTH=""
-FAST-OPEN=""
-WORKERS=""
-PREFER-IPV6="--prefer-ipv6"
+FAST_OPEN=""
+WORKERS=1
+PREFER_IPV6="--prefer-ipv6"
 
-while getopts "s:p:k:m:t:a" OPT; do
+while getopts "s:p:k:m:t:w:af" OPT; do
   case $OPT in
     s)
         SERVER_ADDR=$OPTARG;;
@@ -22,9 +22,13 @@ while getopts "s:p:k:m:t:a" OPT; do
         METHOD=$OPTARG;;
     t)
         TIMEOUT=$OPTARG;;
+    w)
+        WORKERS=$OPTARG;;
     a)
         ONE_TIME_AUTH="-a";;
+    f)
+        FAST_OPEN="--fast-open";;
   esac
 done
 
-/usr/bin/ssserver -s $SERVER_ADDR -p $SERVER_PORT -k $PASSWORD -m $METHOD -t $TIMEOUT $ONE_TIME_AUTH
+/usr/bin/ssserver -s $SERVER_ADDR -p $SERVER_PORT -k $PASSWORD -m $METHOD -t $TIMEOUT -w $WORKERS $ONE_TIME_AUTH $FAST_OPEN $PREFER_IPV6

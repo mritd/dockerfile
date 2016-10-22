97b40f6d5bc238023aac7ccab562a3d255bbda9d
mritd <mritd1234@gmail.com>
2016-10-22 22:23:49 +0800

    增加入口脚本

--- v2ray/entrypoint.sh
+++ v2ray/entrypoint.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+CMD=$1
+CONFIG=$2
+
+if [ "$CMD" != "" ] && [ "$CONFIG" != "" ] && [ "$CMD" != "-c" ]; then
+  echo "$CONFIG" > /etc/v2ray/config.json
+  echo -e "\033[32mUse a custom configuration...\033[0m"
+fi
+
+v2ray -config /etc/v2ray/config.json

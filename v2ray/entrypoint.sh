7565017ae732cf4e023ddad0833d3635fa9a08b9
mritd <mritd1234@gmail.com>
2016-10-22 23:29:43 +0800

    修正 -c 参数错误，禁止命令覆盖

--- v2ray/entrypoint.sh
+++ v2ray/entrypoint.sh
@@ -3,7 +3,7 @@
 CMD=$1
 CONFIG=$2
 
-if [ "$CMD" != "" ] && [ "$CONFIG" != "" ] && [ "$CMD" != "-c" ]; then
+if [ "$CMD" != "" ] && [ "$CONFIG" != "" ] && [ "$CMD" == "-c" ]; then
   echo "$CONFIG" > /root/config.json
   echo -e "\033[32mUse a custom configuration...\033[0m"
 fi

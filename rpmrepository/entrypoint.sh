3b22ea0c7322178cffd13b62da82fcedbec21f78
mritd <mritd1234@gmail.com>
2016-10-27 20:37:11 +0800

    修复路径错误

--- rpmrepository/entrypoint.sh
+++ rpmrepository/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /usr/share/nginx/html/centos/7.2/{i386,x86_64,noarch,updates,SRPMS}
+mkdir -p /usr/share/nginx/html/centos/7/{i386,x86_64,noarch,updates,SRPMS}
 cd /usr/share/nginx/html/centos/7.2/ && ls | xargs -i createrepo {}
 crond
 nginx -g "daemon off;"

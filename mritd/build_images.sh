92e7eb2760b09996add83fffec5f17a6d4bf6b72
mritd <mritd1234@gmail.com>
2016-10-27 22:42:19 +0800

    增加 rpm 仓库镜像build

--- mritd/build_images.sh
+++ mritd/build_images.sh
@@ -4,9 +4,11 @@ if [ "$1" == "-f" ];then
   (cd idea && docker build --no-cache -t mritd/idea .) 
   (cd jekyll && docker build --no-cache -t mritd/jekyll .)
   (cd nginx && docker build --no-cache -t mritd/nginx .)
+  (cd ../rpmrepository && docker build --no-cache -t mritd/rpmrepository .)
 else
   (cd idea && docker build -t mritd/idea .)
   (cd jekyll && docker build -t mritd/jekyll .)
   (cd nginx && docker build -t mritd/nginx .)
+  (cd ../rpmrepository && docker build -t mritd/rpmrepository .)
 fi
 

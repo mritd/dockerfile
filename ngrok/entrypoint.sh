510874c08020f67ab1098754c449b4da4e6932ab
mritd <mritd@mritd.me>
2017-01-23 11:57:35 +0800

    放弃 ngrok

--- ngrok/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-set -e
-
-if [ "" != "$!" ];then
-    ./ngrok $@
-else
-    ./ngrok -tlsKey=/etc/nginx/ssl/mritd.me.key -tlsCrt=/nginx/ssl/mritd.me.crt -domain="ngrok.mritd.me" -httpAddr=":80" -httpsAddr=":443"
-fi

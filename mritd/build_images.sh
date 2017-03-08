cdc9faaf6e6dff57e7baa5ab76d4180dea5352fb
mritd <mritd@mritd.me>
2017-03-08 08:26:33 +0000

    修正编译脚本

--- mritd/build_images.sh
+++ mritd/build_images.sh
@@ -4,11 +4,13 @@ if [ "$1" == "-f" ];then
   (cd idea && docker build --no-cache -t mritd/idea .) 
   (cd jekyll && docker build --no-cache -t mritd/jekyll .)
   (cd nginx && docker build --no-cache -t mritd/nginx .)
-  (cd ../rpmrepository && docker build --no-cache -t mritd/rpmrepository .)
+  (cd gcr-registry && docker build --no-cache -t mritd/gcr.io .)
+  (cd yum && docker build --no-cache -t mritd/yum .)
 else
   (cd idea && docker build -t mritd/idea .)
   (cd jekyll && docker build -t mritd/jekyll .)
   (cd nginx && docker build -t mritd/nginx .)
-  (cd ../rpmrepository && docker build -t mritd/rpmrepository .)
+  (cd gcr-registry && docker build -t mritd/gcr.io .)
+  (cd yum && docker build -t mritd/yum .)
 fi
 

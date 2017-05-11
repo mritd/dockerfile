cc1e386856a28bfecb10c5169578cd576bbf7ab9
mritd <mritd1234@gmail.com>
2017-05-11 19:54:45 +0800

    DMCA

--- mritd/build_images.sh
+++ mritd/build_images.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 if [ "$1" == "-f" ];then
-  (cd idea && docker build --no-cache -t mritd/idea .) 
   (cd jekyll && docker build --no-cache -t mritd/jekyll .)
   (cd nginx && docker build --no-cache -t mritd/nginx .)
   (cd gcr-registry && docker build --no-cache -t mritd/gcr.io .)
   (cd yum && docker build --no-cache -t mritd/yum .)
 else
-  (cd idea && docker build -t mritd/idea .)
   (cd jekyll && docker build -t mritd/jekyll .)
   (cd nginx && docker build -t mritd/nginx .)
   (cd gcr-registry && docker build -t mritd/gcr.io .)

ce188b7d651853ad6fe7f37d7f42741109a80ff8
mritd <mritd1234@gmail.com>
2017-05-20 18:56:09 +0800

    增加日志输出、选项支持

--- time-machine/entrypoint.sh
+++ time-machine/entrypoint.sh
@@ -5,9 +5,23 @@ PASSWORD=${PASSWORD:-"123456"}
 MOUNT_POINT=${MOUNT_POINT-"/data"}
 VOL_SIZE_MB=${VOL_SIZE_MB-"512000"}
 
+while getopts "u:p:m:v" OPT; do
+    case $OPT in
+        u)
+            USERNAME=$OPTARG;;
+        p)
+            PASSWORD=$OPTARG;;
+        m)
+            MOUNT_POINT=$OPTARG;;
+        v)
+            VOL_SIZE_MB=$OPTARG;;
+    esac
+done
+
 cat /etc/passwd | grep $USERNAME >& /dev/null
 
 if [ $? -ne 0 ];then
+    echo "Add user: $USERNAME..."
     adduser -S -H -G root $USERNAME
 else
     echo "User: $USERNAME already exists!"
@@ -19,6 +33,7 @@ chown -R $USERNAME:root $MOUNT_POINT
 cat /etc/afp.conf | grep "$USERNAME" >& /dev/null
 
 if [ $? -ne 0 ];then
+    echo "Update /etc/afp.conf..."
     cat << EOF >> /etc/afp.conf
 [$USERNAME]
 valid users = $USERNAME
@@ -34,4 +49,5 @@ if [ ! -e /var/run/dbus/system_bus_socket ]; then
     dbus-daemon --system
 fi
 
+echo "Netatalk starting..."
 netatalk -d

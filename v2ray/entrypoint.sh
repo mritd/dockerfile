d44e54314ae902f4fab5c0a1a9b1666d54e8a8db
mritd <mritd@mritd.me>
2017-06-13 23:58:58 +0900

    支持自定义命令

--- v2ray/entrypoint.sh
+++ v2ray/entrypoint.sh
@@ -6,6 +6,8 @@ CONFIG=$2
 if [ "$CMD" != "" ] && [ "$CONFIG" != "" ] && [ "$CMD" == "-c" ]; then
   echo "$CONFIG" > /etc/v2ray/config.json
   echo -e "\033[32mUse a custom configuration...\033[0m"
+elif [ "$CMD" != "" ] && [ "$CMD" != "-c" ]; then
+  $*
+else
+  v2ray -config /etc/v2ray/config.json
 fi
-
-v2ray -config /etc/v2ray/config.json

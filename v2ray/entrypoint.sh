41aa752e566128f3261c609cf05621cc361d49c5
mritd <mritd1234@gmail.com>
2017-06-13 22:36:25 +0800

    规范 Dockerfile

--- v2ray/entrypoint.sh
+++ v2ray/entrypoint.sh
@@ -4,8 +4,8 @@ CMD=$1
 CONFIG=$2
 
 if [ "$CMD" != "" ] && [ "$CONFIG" != "" ] && [ "$CMD" == "-c" ]; then
-  echo "$CONFIG" > /root/config.json
+  echo "$CONFIG" > /etc/config.json
   echo -e "\033[32mUse a custom configuration...\033[0m"
 fi
 
-/root/v2ray -config /root/config.json
+v2ray -config /etc/config.json

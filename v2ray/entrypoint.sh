10ed1a33b7c31a13ab84f0e09fee99bc3052011b
mritd <mritd1234@gmail.com>
2017-08-13 11:29:18 +0800

    fix entrypoint

--- v2ray/entrypoint.sh
+++ v2ray/entrypoint.sh
@@ -3,10 +3,12 @@
 CMD=$1
 CONFIG=$2
 
-if [ "$CMD" != "" ] && [ "$CONFIG" != "" ] && [ "$CMD" == "-c" ]; then
+if [ "$CONFIG" != "" ] && [ "$CMD" == "-c" ]; then
   echo "$CONFIG" > /etc/v2ray/config.json
   echo -e "\033[32mUse a custom configuration...\033[0m"
-elif [ "$CMD" != "" ] && [ "$CMD" != "-c" ]; then
+fi
+
+if [ "$CMD" != "" ] && [ "$CMD" != "-c" ]; then
   $*
 else
   v2ray -config /etc/v2ray/config.json

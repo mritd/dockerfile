71e43ee374cbeb1dafb2c58cecb37ca0ca38da67
mritd <mritd1234@gimail.com>
2018-01-25 14:02:56 +0900

    :sparkles: feat(mritd): 调整网站
    调整网站
    
    Signed-off-by: mritd <mritd1234@gimail.com>

--- mritd/build_images.sh
+++ mritd/build_images.sh
@@ -3,12 +3,10 @@
 if [ "$1" == "-f" ];then
   (cd jekyll && docker build --no-cache -t mritd/jekyll .)
   (cd nginx && docker build --no-cache -t mritd/nginx .)
-  (cd gcr-registry && docker build --no-cache -t mritd/gcr.io .)
   (cd yum && docker build --no-cache -t mritd/yum .)
 else
   (cd jekyll && docker build -t mritd/jekyll .)
   (cd nginx && docker build -t mritd/nginx .)
-  (cd gcr-registry && docker build -t mritd/gcr.io .)
   (cd yum && docker build -t mritd/yum .)
 fi
 

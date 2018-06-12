149d108ba4fb04feaaa699a58d51a3190104df19
mritd <mritd1234@gmail.com>
2018-06-12 17:50:05 +0900

    fix(param): fix mt prefix
    fix mt prefix
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- mtproxyweb/entrypoint.sh
+++ mtproxyweb/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-sed -i "s@{{ADDRESS}}@${ADDRESS}@g" /usr/share/nginx/html/index.html
-sed -i "s@{{PORT}}@${PORT}@g" /usr/share/nginx/html/index.html
-sed -i "s@{{SECRET}}@${SECRET}@g" /usr/share/nginx/html/index.html
+sed -i "s@{{MT_ADDRESS}}@${MT_ADDRESS}@g" /usr/share/nginx/html/index.html
+sed -i "s@{{MT_PORT}}@${MT_PORT}@g" /usr/share/nginx/html/index.html
+sed -i "s@{{MT_SECRET}}@${MT_SECRET}@g" /usr/share/nginx/html/index.html
 
 nginx -g "daemon off;"

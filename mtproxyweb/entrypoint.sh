967e756b424c9ca77a1a848ab39c591d29535dc5
mritd <mritd1234@gmail.com>
2018-06-12 17:24:00 +0900

    fix(mtproxyweb): fix
    fix
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- mtproxyweb/entrypoint.sh
+++ mtproxyweb/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-sed -i "s@{{ADDRESS}}@${SECRET}@g" /usr/share/nginx/html/index.html
-sed -i "s@{{PORT}}@${SECRET}@g" /usr/share/nginx/html/index.html
+sed -i "s@{{ADDRESS}}@${ADDRESS}@g" /usr/share/nginx/html/index.html
+sed -i "s@{{PORT}}@${PORT}@g" /usr/share/nginx/html/index.html
 sed -i "s@{{SECRET}}@${SECRET}@g" /usr/share/nginx/html/index.html
 
 nginx -g "daemon off;"

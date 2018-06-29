0eecec805f5811d45cd14a6f82653c36736b8e34
mritd <mritd1234@gmail.com>
2018-06-29 17:07:18 +0800

    fix(config): fix config
    fix config
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- logstash/docker-entrypoint.sh
+++ logstash/docker-entrypoint.sh
@@ -6,15 +6,8 @@ env2yaml ${LOGSTASH_HOME}/config/logstash.yml
 
 chown -R logstash:logstash /data /var/log/logstash 
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-    set -- logstash "$@"
+if [[ -z $1 ]] || [[ ${1:0:1} == '-' ]] ; then
+  	exec logstash "$@"
+else
+  	exec "$@"
 fi
-
-# Run as user "logstash" if the command is "logstash"
-# allow the container to be started with `--user`
-if [ "$1" = 'logstash' -a "$(id -u)" = '0' ]; then
-    set -- su-exec logstash "$@"
-fi
-
-exec "$@"

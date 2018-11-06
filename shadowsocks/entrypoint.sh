450e698a174a7ef84f1ae2362917af7e75c9d4fd
mritd <mritd1234@gmail.com>
2018-11-06 15:57:10 +0800

    fix(random-numbers): fix 'system doesn't provide enough entropy' error
    fix 'system doesn't provide enough entropy' error
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- shadowsocks/entrypoint.sh
+++ shadowsocks/entrypoint.sh
@@ -5,8 +5,9 @@ SS_MODULE=${SS_MODULE:-"ss-server"}
 KCP_CONFIG=${KCP_CONFIG:-""}
 KCP_MODULE=${KCP_MODULE:-"kcpserver"}
 KCP_FLAG=${KCP_FLAG:-"false"}
+RNGD_FLAG=${RNGD_FLAG:-"false"}
 
-while getopts "s:m:k:e:x" OPT; do
+while getopts "s:m:k:e:xr" OPT; do
     case $OPT in
         s)
             SS_CONFIG=$OPTARG;;
@@ -18,19 +19,26 @@ while getopts "s:m:k:e:x" OPT; do
             KCP_MODULE=$OPTARG;;
         x)
             KCP_FLAG="true";;
+        r)
+            RNGD_FLAG="true";;
     esac
 done
 
-if [ "$KCP_FLAG" == "true" ] && [ "$KCP_CONFIG" != "" ]; then
+if [ "${RNGD_FLAG}" == "true" ]; then
+    echo -e "\033[32mUse /dev/urandom to quickly generate high-quality random numbers......\033[0m"
+    rngd -r /dev/urandom
+fi
+
+if [ "${KCP_FLAG}" == "true" ] && [ "${KCP_CONFIG}" != "" ]; then
     echo -e "\033[32mStarting kcptun......\033[0m"
-    $KCP_MODULE $KCP_CONFIG 2>&1 &
+    ${KCP_MODULE} ${KCP_CONFIG} 2>&1 &
 else
     echo -e "\033[33mKcptun not started......\033[0m"
 fi
 
-if [ "$SS_CONFIG" != "" ]; then
+if [ "${SS_CONFIG}" != "" ]; then
     echo -e "\033[32mStarting shadowsocks......\033[0m"
-    $SS_MODULE $SS_CONFIG
+    ${SS_MODULE} ${SS_CONFIG}
 else
     echo -e "\033[31mError: SS_CONFIG is blank!\033[0m"
     exit 1

ba8751a8cdf3a76496ed1b3e372d7ec645b06ffc
mritd <mritd1234@gmail.com>
2018-12-18 21:39:50 +0800

    feat(shadowsocks2): remove option
    remove option
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- shadowsocks2/entrypoint.sh
+++ shadowsocks2/entrypoint.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-SS_CONFIG=${SS_CONFIG:-""}
-
-while getopts ":s:" OPT; do
-    case $OPT in
-        s)
-            SS_CONFIG=$OPTARG;;
-    esac
-done
-
 if [ "${SS_CONFIG}" != "" ]; then
-    echo -e "\033[32mStarting shadowsocks......\033[0m"
     shadowsocks -s ${SS_CONFIG} -verbose
 else
     shadowsocks $@

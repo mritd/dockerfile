c5b7a4ee29ccd51e96c6b4b804c1322cc9a14bcc
mritd <mritd1234@gmail.com>
2018-12-26 17:59:38 +0800

    feat(webqq): remove webqq
    remove webqq
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- webqq/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-perl -MMojo::Webqq -e 'Mojo::Webqq->new(log_encoding=>"utf8")->load(["ShowMsg","UploadQRcode"])->load("Openqq",data=>{listen=>[{port=>$ENV{MOJO_WEBQQ_PLUGIN_OPENQQ_PORT}//5000}],post_api=>$ENV{MOJO_WEBQQ_PLUGIN_OPENQQ_POST_API}})->run'

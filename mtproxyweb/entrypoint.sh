d6ac426aea51776c349df5bfc8d3f2ab9a3f5c96
mritd <mritd@linux.com>
2019-06-13 11:23:49 +0800

    feat(mtproxyweb): remove mtproxyweb
    remove mtproxyweb
    
    Signed-off-by: mritd <mritd@linux.com>

--- mtproxyweb/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sed -i "s@{{MT_ADDRESS}}@${MT_ADDRESS}@g" /usr/share/nginx/html/index.html
-sed -i "s@{{MT_PORT}}@${MT_PORT}@g" /usr/share/nginx/html/index.html
-sed -i "s@{{MT_SECRET}}@${MT_SECRET}@g" /usr/share/nginx/html/index.html
-
-nginx -g "daemon off;"

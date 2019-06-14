58e578d637b1e4f950704272ece648d9a210ac1f
mritd <mritd@linux.com>
2019-06-14 22:43:49 +0800

    feat(lgtm): remove lgtm
    remove lgtm
    
    Signed-off-by: mritd <mritd@linux.com>

--- lgtm/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-lgtm -token $LGTM_TOKEN \
-     -gitlab_url $LGTM_GITLAB_URL \
-     -lgtm_count $LGTM_COUNT \
-     -lgtm_note $LGTM_NOTE \
-     -log_level $LGTM_LOG_LEVEL \
-     -db_path $LGTM_DB_PATH \
-     -port $LGTM_PORT

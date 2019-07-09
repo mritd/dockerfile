264d3403c791ab68f7fe5bc39304009e55fa7647
mritd <mritd@linux.com>
2019-07-09 17:27:09 +0800

    feat(confluence): add SECREC_SMTP support
    add SECREC_SMTP support
    
    Signed-off-by: mritd <mritd@linux.com>

--- atlassian-confluence/hijack.sh
+++ atlassian-confluence/hijack.sh
@@ -2,4 +2,11 @@
 
 export JAVA_OPTS="${JAVA_OPTS} -javaagent:${AGENT_PATH}"
 
+# If you want to use SECURE_SMTP, mount the /opt/atlassian/confluence/conf directory
+# and follow the link documentation to modify the configuration.
+# refs https://confluence.atlassian.com/doc/setting-up-a-mail-session-for-the-confluence-distribution-6328.html
+if [ "${JNDI_EMAIL}" == "true" ]; then
+    mv ${CONFLUENCE_INSTALL_DIR}/confluence/WEB-INF/lib/mail-*.jar ${CONFLUENCE_INSTALL_DIR}/lib/
+fi
+
 /entrypoint.sh -fg

2e8ba3eb463dfa477caa9afd33be82b1af0a0b25
mritd <mritd@linux.com>
2019-07-21 10:15:17 +0800

    feat(ss): support log to file
    support log to file
    
    Signed-off-by: mritd <mritd@linux.com>

--- shadowsocks/entrypoint.sh
+++ shadowsocks/entrypoint.sh
@@ -5,8 +5,9 @@ SS_MODULE=${SS_MODULE:-"ss-server"}
 KCP_CONFIG=${KCP_CONFIG:-""}
 KCP_MODULE=${KCP_MODULE:-"kcpserver"}
 KCP_FLAG=${KCP_FLAG:-"false"}
+LOG_FILE=${LOG_FILE:-"/dev/stdout"}
 
-while getopts "s:m:k:e:x" OPT; do
+while getopts "s:m:k:e:o:x" OPT; do
     case $OPT in
         s)
             SS_CONFIG=$OPTARG;;
@@ -16,6 +17,8 @@ while getopts "s:m:k:e:x" OPT; do
             KCP_CONFIG=$OPTARG;;
         e)
             KCP_MODULE=$OPTARG;;
+        o)
+            LOG_FILE=$OPTARG;;
         x)
             KCP_FLAG="true";;
     esac
@@ -25,6 +28,7 @@ export SS_CONFIG=${SS_CONFIG}
 export SS_MODULE=${SS_MODULE}
 export KCP_CONFIG=${KCP_CONFIG}
 export KCP_MODULE=${KCP_MODULE}
+export LOG_FILE=${LOG_FILE}
 export KCP_FLAG=${KCP_FLAG}
 
 exec runsvdir -P /etc/service

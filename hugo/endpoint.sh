ce384cdb87aee42186fc8211931e2b2ffe871352
mritd <mritd@linux.com>
2020-01-20 21:48:47 +0800

    feat(hugo): update hugo
    update hugo
    
    Signed-off-by: mritd <mritd@linux.com>

--- hugo/endpoint.sh
+++ hugo/endpoint.sh
@@ -6,4 +6,5 @@ git clone -b gh-pages https://github.com/mritd/mritd.com.git /usr/share/nginx/ht
 
 echo "blog starting..."
 
+httpcmd -r ^git.* -t ${HTTPCMD_TOKEN} -w /usr/share/nginx/html -d
 nginx -g "daemon off;"

c3140d4cd5b3066d9be2cd6922cd5839a6d96e0f
mritd <mritd@linux.com>
2020-02-06 20:59:27 +0800

    feat(fastdfs): add fastdfs
    add fastdfs
    
    Signed-off-by: mritd <mritd@linux.com>

---
+++ fastdfs/entrypoint.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+MODE=$1
+
+if [ "${MODE}" == "trackerd" ];then
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] INFO: container run as trackerd mode."
+    /etc/init.d/fdfs_trackerd start
+    while true;do
+        if [ ! -n "$(ps -ef | grep fdfs_trackerd | grep -v grep)" ];then
+            echo "[$(date '+%Y-%m-%d %H:%M:%S')] ERROR: fdfs health check failed, container exit!"
+            exit 1
+        fi
+        sleep 1
+    done
+elif [ "${MODE}" == "storaged" ];then
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] INFO: container run as storaged mode."
+    /etc/init.d/fdfs_storaged start
+    while true;do
+        if [ ! -n "$(ps -ef | grep fdfs_storaged | grep -v grep)" ];then
+            echo "[ $(date '+%Y-%m-%d %H:%M:%S') ] ERROR: fdfs health check failed, container exit!"
+            exit 1
+        fi
+        sleep 1
+    done
+else
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] INFO: container run as test mode."
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] WARN: fastdfs container must use host network mode."
+
+    if [ ! -n "${PUBIP}" ];then
+        echo "[ $(date '+%Y-%m-%d %H:%M:%S') ] ERROR: Environment variable \"PUBIP\" is not set!"
+        exit 1
+    fi
+    
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] INFO: use ${PUBIP} as fastdfs ip."
+    sed -i "s@{{TRACKER_IP}}@${PUBIP}@g" /etc/fdfs/*.conf
+
+    /etc/init.d/fdfs_trackerd start
+    /etc/init.d/fdfs_storaged start
+
+    while true;do
+        if [ ! -n "$(ps -ef | grep fdfs_trackerd | grep -v grep)" ] || \
+            [ ! -n "$(ps -ef | grep fdfs_storaged | grep -v grep)" ];then
+            echo "[$(date '+%Y-%m-%d %H:%M:%S')] ERROR: fdfs health check failed, container exit!"
+            exit 1
+        fi
+        sleep 1
+    done
+fi
